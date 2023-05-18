dfb24f91c48a2c862b355ad32476042ef08815d1
Hasnat Ali <hasnat.ali@weatherwalay.com>
2023-05-18 14:32:56 +0500

    test

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 const Contact = () => {
+  const [isFeedback, setIsFeedback] = useState(false);
+  async function handleSubmit(e) {
+    e.preventDefault();
+    var { name, email, message } = e.target.elements;
+    name = name.value;
+    message = message.value;
+    email = email.value;
+    document.getElementById("myForm").reset();
+    setIsFeedback(true);
+    setTimeout(() => setIsFeedback(false), 5000);
+  }
   return (
     <>
       <Navbar />
@@ -43,45 +54,54 @@ const Contact = () => {
               </div>
             </div>
             <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-              <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
-                Feedback
-              </h2>
+              {!isFeedback ? (
+                <form id="myForm" onSubmit={handleSubmit}>
+                  <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
+                    Feedback
+                  </h2>
 
-              <div className="relative mb-4">
-                <label for="name" class="leading-7 text-sm text-gray-600">
-                  Name
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-              </div>
-              <div className="relative mb-4">
-                <label for="email" class="leading-7 text-sm text-gray-600">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                />
-              </div>
-              <div className="relative mb-4">
-                <label for="message" class="leading-7 text-sm text-gray-600">
-                  Message
-                </label>
-                <textarea
-                  id="message"
-                  name="message"
-                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                ></textarea>
-              </div>
-              <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                submit
-              </button>
+                  <div className="relative mb-4">
+                    <label for="name" class="leading-7 text-sm text-gray-600">
+                      Name
+                    </label>
+                    <input
+                      type="text"
+                      id="name"
+                      name="name"
+                      className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    />
+                  </div>
+                  <div className="relative mb-4">
+                    <label for="email" class="leading-7 text-sm text-gray-600">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    />
+                  </div>
+                  <div className="relative mb-4">
+                    <label
+                      for="message"
+                      class="leading-7 text-sm text-gray-600"
+                    >
+                      Message
+                    </label>
+                    <textarea
+                      id="message"
+                      name="message"
+                      className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                    ></textarea>
+                  </div>
+                  <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                    submit
+                  </button>
+                </form>
+              ) : (
+                <h1>ThankYou for your feedback</h1>
+              )}
             </div>
           </div>
         </section>
